3f660bc3a0472339ad48fe452458dc9c7b18f11d
TimorLapin <38160851+TimorLapin@users.noreply.github.com>
2018-05-08 20:42:02 +0000

    Update getfile.py

--- getfile.py
+++ getfile.py
@@ -4,18 +4,19 @@ import time
 import multiprocessing # eller multithreading
 
 
-firebase = firebase.FirebaseApplication('https://heliot-f77cc.firebaseio.com/')
-blom1_tid = firebase.get('/blom1','tim')
-blom1_min = firebase.get('/blom1','min')
-blom1_dur = firebase.get('/blom1','dur')
+def get_data()
+    firebase = firebase.FirebaseApplication('https://heliot-f77cc.firebaseio.com/')
+    blom1_tid = firebase.get('/blom1','tim')
+    blom1_min = firebase.get('/blom1','min')
+    blom1_dur = firebase.get('/blom1','dur')
 
-blom2_tid = firebase.get('/blom2','tim')
-blom2_min = firebase.get('/blom2','min')
-blom2_dur = firebase.get('/blom2','dur')
+    blom2_tid = firebase.get('/blom2','tim')
+    blom2_min = firebase.get('/blom2','min')
+    blom2_dur = firebase.get('/blom2','dur')
 
-blom3_tid = firebase.get('/blom3','tim')
-blom3_min = firebase.get('/blom3','min')
-blom3_dur = firebase.get('/blom3','dur')
+    blom3_tid = firebase.get('/blom3','tim')
+    blom3_min = firebase.get('/blom3','min')
+    blom3_dur = firebase.get('/blom3','dur')
 
 #result4 = firebase.post('/blom1',{'dur':'333'})
 #result5 = firebase.put('/blom1','dur',333)
@@ -28,14 +29,21 @@ print(blom1_tid,blom1_min,blom1_dur)
 def duration(dur)                   # ex. dur = 2000
     antal_min = dur/25 = 80         # Man vill vattna med exempelvis 2000 ml
     brak_tal = 80/60 = 1,33
-    while brak_tal>1
-        brak_tal = brak_tal-1       # 1,33 - 1 = 0,33
-        timmar+=1                   # timmmar = 1
-    minuter = brak_tal * 60         # minuter = 20
     
+    if brak_tal >1
+        while brak_tal>1
+            brak_tal = brak_tal-1       # 1,33 - 1 = 0,33
+            timmar+=1                   # timmmar = 1
+        minuter = brak_tal * 60         # minuter = 20
+    else 
+        minuter = brak_tal * 60
+        
+        
 # Det vi får här är, 1 timme och 20 min. Tiden som vi vill ha en ventil öppen. Som vi sedan ställer mot vår time-funktion.
 # Om vi använder multiprocessing/multithreading då kan vi ha en delay i varje funktion på det antal sekunder som motsvarar bevattningen. 
 # ex. om dur = 1500ml/tim då blir delay = 3600 sekunder. Dela med 25, sen gånger 60
+
+
 while True:
     
         tid = time.asctime()
@@ -47,18 +55,21 @@ while True:
         if tid2==tidblom1:
             k = multiprocessing.Process(target=timetest.kruka_1, args(blom1_dur,))
             k.start()
-            k.join()
-
+            #k.join()
+            avisering = firebase.put('/blom1','dur',tid)    # Avisering efter senaste bevattning
+            
         if tid2==tidblom2:
             k = multiprocessing.Process(target=timetest.kruka_2, args(blom2_dur,))            
             k.start()            
-            k.join()
-
+            #k.join()
+            avisering = firebase.put('/blom1','dur',tid)    # Avisering efter senaste bevattning
+        
         if tid2==tidblom3:
             k = multiprocessing.Process(target=timetest.kruka_3, args(blom3_dur,))            
             k.start()            
-            k.join()
-
-
-       # time.sleep(100)
+            #k.join()
+            avisering = firebase.put('/blom1','dur',tid)    # Avisering efter senaste bevattning
+        
+        get_data()                          # Hämta data igen
+       
             
